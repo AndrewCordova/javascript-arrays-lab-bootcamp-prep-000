ec586eb1878037800119bd057a27169cfdfc4630
Andrew Cordova <andrewcordova13@yahoo.com>
2017-09-11 22:09:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-var kittens = ["milo", "Otis", "Garfield"]
+var kittens = ["milo", "Otis", "Garfield"];
 
 destructivelyAppendKitten(element){
   kittens.push("Kitty")
